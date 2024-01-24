b608ed3e4733f7b910d7c97b1be50930bdf7b45c
sameer <sameerbhardiya.com>
2024-01-24 23:58:35 +0530

    new

---
+++ client/src/App.js
@@ -0,0 +1,74 @@
+import React,{useMemo, useState} from 'react';
+import styled from "styled-components";
+import {MainLayout} from './styled/Layout'
+import Orb from "./components/orb/Orb";
+import Navigation from "./components/Navigation/Navigation";
+import Dashboard from './components/Dashboard/Dashboard';
+import Income from './components/Income/Income';
+import Expenses from './components/Expense/Expense';
+import { useGlobalContext } from './context/globalContext';
+
+function App() {
+
+  
+ 
+  const [active,setActive] = useState(1)
+
+  const global = useGlobalContext()
+  console.log(global);
+
+  const displayData = () =>{
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4: 
+        return <Expenses />
+      default: 
+        return <Dashboard />
+    }
+  }
+
+  const orbMemo = useMemo(() =>{
+    return <Orb/>
+  },[])
+  
+  return (
+    <AppStyled className="App">
+      {orbMemo}
+      <MainLayout>
+        <Navigation active={active} setActive={setActive} />
+        <main>
+        {displayData()}
+        </main>
+      </MainLayout>
+    </AppStyled>
+  );
+}
+const AppStyled = styled.div`
+height: 100vh;
+background-color:white;
+position:relative;
+main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
+`;
+
+
+
+
+
+
+
+export default App;
